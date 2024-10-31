0800c0026647ae788fdfdfbbaaa5799c38ff7319
Ghifary <mhghifaryy@gmail.com>
2024-10-31 09:57:05 +0700

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,11 +1,12 @@
 Import-Module PSReadLine
 
 # I modded this:
-oh-my-posh init pwsh --config 'C:\Users\mhghi\AppData\Local\Programs\oh-my-posh\themes\atomicBit.omp.json' | Invoke-Expression
+# oh-my-posh init pwsh --config 'C:\Users\mhghi\AppData\Local\Programs\oh-my-posh\themes\atomicBit.omp.json' | Invoke-Expression
 
-# oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/Ghifaryh/gip-ohmyposh/main/atomicBit-gipmodified-new.omp.json'| Invoke-Expression
+oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/Ghifaryh/gip-ohmyposh/main/atomicBit-gipmodified-new.omp.json'| Invoke-Expression
 
-oh-my-posh init pwsh --config ~/atomicBit.omp.json | Invoke-Expression
+
+# oh-my-posh init pwsh --config ~/atomicBit.omp.json | Invoke-Expression
 
 # oh-my-posh init pwsh --config 'C:\Users\mhghi\AppData\Local\Programs\oh-my-posh\themes\ys.omp.json' | Invoke-Expression
 
@@ -27,8 +28,31 @@ function NpmDev {
 
 Set-Alias nrd NpmDev
 
+
 function wafTIK {
     ssh gip@192.168.255.117
 }
 
 Set-Alias -Name tikwaf -Value wafTIK
+
+function giproj { cd D:\Stuff\Projects }
+
+Set-Alias -Name project -Value giproj
+
+function open-laragon {
+    Start-Process "C:\laragon\laragon.exe"
+}
+
+Set-Alias -Name laragon -Value open-laragon
+
+
+
+# oh-my-posh init pwsh --config 'C:\Users\mhghi\AppData\Local\Programs\oh-my-posh\themes\craver.omp.json' | Invoke-Expression
+
+# oh-my-posh init pwsh --config 'C:\Users\mhghi\AppData\Local\Programs\oh-my-posh\themes\tonybaloney.omp.json' | Invoke-Expression
+
+# oh-my-posh init pwsh --config 'C:\Users\mhghi\AppData\Local\Programs\oh-my-posh\themes\the-unnamed.omp.json' | Invoke-Expression
+
+# oh-my-posh init pwsh --config 'C:\Users\mhghi\AppData\Local\Programs\oh-my-posh\themes\chips.omp.json' | Invoke-Expression
+
+
