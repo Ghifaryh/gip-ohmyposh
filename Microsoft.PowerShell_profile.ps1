1bc8f5eedc4a52a2bd11f3ca3727d59fba60a23a
Ghifary <mhghifaryy@gmail.com>
2026-06-03 09:40:50 +0700

    Refactor PowerShell profile with new configurations

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,66 +1,38 @@
 Import-Module PSReadLine
 
-# I modded this:
-# oh-my-posh init pwsh --config 'C:\Users\mhghi\AppData\Local\Programs\oh-my-posh\themes\atomicBit.omp.json' | Invoke-Expression
+# --- 1. Linux/Fish-like Terminal Experience ---
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle Inline
+Set-PSReadLineOption -Colors @{ InlinePrediction = '#606060' }
+Set-PSReadLineOption -EditMode Windows
 
-oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/Ghifaryh/gip-ohmyposh/main/atomicBit-gipmodified-new.omp.json'| Invoke-Expression
+# --- 2. Intuitive Key Bindings ---
+Set-PSReadLineKeyHandler -Chord "RightArrow" -Function AcceptSuggestion
+Set-PSReadLineKeyHandler -Chord "Ctrl+RightArrow" -Function ForwardWord
+Set-PSReadLineKeyHandler -Chord "Tab" -Function MenuComplete
+Set-PSReadLineKeyHandler -Chord "UpArrow" -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Chord "DownArrow" -Function HistorySearchForward
 
+# --- 3. Prompt Theme (Oh-My-Posh) ---
+oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/Ghifaryh/gip-ohmyposh/main/atomicBit-gipmodified-new.omp.json' | Invoke-Expression
 
-# oh-my-posh init pwsh --config ~/atomicBit.omp.json | Invoke-Expression
-
-# oh-my-posh init pwsh --config 'C:\Users\mhghi\AppData\Local\Programs\oh-my-posh\themes\ys.omp.json' | Invoke-Expression
-
-# oh-my-posh init pwsh --config 'C:\Users\mhghi\AppData\Local\Programs\oh-my-posh\themes\takuya.omp.json' | Invoke-Expression
-
-Set-PSReadLineKeyHandler -Chord "Tab" -Function AcceptSuggestion
-
-Set-PSReadLineKeyHandler -Chord "RightArrow" -Function ForwardWord
-
-# Set-PSReadLineOption -PredictionViewStyle ListView
-
+# --- 4. Aliases & Custom Functions ---
 Set-Alias -Name g -Value git
 
-# clear-host
-
-function NpmDev {
-  npm run dev
-}
-
+function NpmDev { npm run dev }
 Set-Alias nrd NpmDev
 
-
-function wafTIK {
-    ssh gip@192.168.255.117
-}
-
+function wafTIK { ssh gip@192.168.255.117 }
 Set-Alias -Name tikwaf -Value wafTIK
 
 function giproj { cd D:\Stuff\Projects }
-
 Set-Alias -Name project -Value giproj
 
-function open-laragon {
-    Start-Process "C:\laragon\laragon.exe"
-}
-
+function open-laragon { Start-Process "C:\laragon\laragon.exe" }
 Set-Alias -Name laragon -Value open-laragon
 
 function gip-gitlab {
      git config --local user.email "mhghifary@itenas.ac.id"
      git config --get user.email
 }
-
 Set-Alias -Name giplab -Value gip-gitlab
-
-# $email = git config --get user.email
-# Write-Output $email
-
-# oh-my-posh init pwsh --config 'C:\Users\mhghi\AppData\Local\Programs\oh-my-posh\themes\craver.omp.json' | Invoke-Expression
-
-# oh-my-posh init pwsh --config 'C:\Users\mhghi\AppData\Local\Programs\oh-my-posh\themes\tonybaloney.omp.json' | Invoke-Expression
-
-# oh-my-posh init pwsh --config 'C:\Users\mhghi\AppData\Local\Programs\oh-my-posh\themes\the-unnamed.omp.json' | Invoke-Expression
-
-# oh-my-posh init pwsh --config 'C:\Users\mhghi\AppData\Local\Programs\oh-my-posh\themes\chips.omp.json' | Invoke-Expression
-
-
